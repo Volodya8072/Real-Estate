3063b05e636010019fa9b5a6d398e3b2b7eb385c
Your Name <you@example.com>
2025-05-09 23:13:43 +0300

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { motion } from "framer-motion"
+import { assets } from '../assets/assets'
+import header from '../assets/header.png'
+
 
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'
-     style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
+     style={{backgroundImage: `url(${header})`}} id='Header'>
         <Navbar/>
         <motion.div
         initial={{opacity: 0, y:100}}
